9eed8a5f1dba0677d17fc513a74f441680692c47
Nathan B <dewakik@gmail.com>
2022-04-08 11:06:00 +0200

    :sparkles: Add json file write

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 const express = require('express')
 const app = express()
 const compression = require('compression')
+const res = require('express/lib/response')
 require('dotenv').config()
 const PORT = process.env.PORT || 8080
 
+const fs = require('fs')
+const req = require('express/lib/request')
+
+
 app.set('view engine', 'ejs')
 app.set('views', 'views')
+
+app.use(express.json()) //Built-in body parser express 4.16+
+app.use(express.urlencoded({extended: true})) //Parse URL-encoded bodies 
 
 app.use(express.static(__dirname + '/public'))
 app.use(compression({level: 6}))
 
 app.get('/', async (request, response) => {
   response.render('index')
+})
+
+app.post('/submitSurvey', async (request, response) => {
+  console.log(request.body)
+  const path = './public/jason.json'
+
+  //Local instance of database aka sketchy JSON files
+  return await fs.writeFile(path, JSON.stringify({userId: request.body.studentId, userData: request.body}), (err, result) => {
+    if (err) return console.log('error at writing',err)
+    return response.render('confirm')
+  })
 })
 
 app.listen(PORT, ()=>{
